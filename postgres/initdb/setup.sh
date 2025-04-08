aad2b15099656e415c700235c601f7ff4c6a85b4
Schadrack Niyibizi <schadrack@cenfri.org>
2025-04-08 22:59:09 +0200

    updated command for overriding initd for postgres service

--- postgres/initdb/setup.sh
+++ postgres/initdb/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Allow all remote connections to the database
+# Configure PostgreSQL to listen on all interfaces
+echo "listen_addresses = '*'" >> /var/lib/postgresql/data/postgresql.conf
+
+# Update access permissions
 echo "host    all             all             0.0.0.0/0               md5" >> /var/lib/postgresql/data/pg_hba.conf
-# Reload PostgreSQL configuration
+
+# Reload configuration
 pg_ctl reload -D /var/lib/postgresql/data
