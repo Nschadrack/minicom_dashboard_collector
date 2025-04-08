7b6c0304d6670672413a9c16e76d31a3dbcaa9f8
Schadrack Niyibizi <schadrack@cenfri.org>
2025-04-08 14:40:41 +0200

    changed wsgi to match in the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ python manage.py showmigrations
 
 python manage.py collectstatic --no-input
 
-gunicorn alta.wsgi:application --bind 0.0.0.0:8000
+gunicorn minicom_dashboard.wsgi:application --bind 0.0.0.0:8000
 # python manage.py runserver 0.0.0.0:8000
