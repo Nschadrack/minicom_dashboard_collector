62a921c4ab5ded8c42e38550f80988d798ad46cb
Schadrack Niyibizi <schadrack@cenfri.org>
2025-04-11 13:53:47 +0200

    forcing background tasks

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,13 @@ python manage.py showmigrations
 python manage.py collectstatic --no-input
 
 # Then start the Gunicorn server
-gunicorn --workers $NUM_WORKERS --bind 0.0.0.0:8000 --timeout 300 minicom_dashboard.wsgi:application
+gunicorn --workers $NUM_WORKERS --bind 0.0.0.0:8000 --timeout 300 minicom_dashboard.wsgi:application &
 
-python manage.py process_tasks # background tasks queue
+sleep 30 # wait for 30 seconds
+
+echo "Going to start background task processor"
+
+python manage.py process_tasks & # background tasks queue
 
 ech "===================================="
 echo "Started background task queue"
